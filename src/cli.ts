0942fe868e5f8432114cc0f01ca07f5f3db31146
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-08-24 14:25:32 +0900

    ダウンロード先のURLデータを格納する

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,7 @@
 import { program, Command } from 'commander';
 import packageJson from '../package.json';
+import XLSX from 'xlsx';
+import path from 'path'
 import { config } from 'dotenv';
 config();
 
@@ -13,6 +15,13 @@ dataCommand
   .command('download')
   .description('')
   .action(async (options: any) => {
+    const workbook = XLSX.readFile(path.join('resources', 'master-data', 'download-file-info.csv'));
+    const sheetNames = Object.keys(workbook.Sheets);
+    const themeRows = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[0]]);
+    const downloadUrls = themeRows.map((themeRow: any) => {
+      return themeRow.url
+    })
+    console.log(downloadUrls);
     console.log('data:download');
   });
 
