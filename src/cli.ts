ff9e7daddba73cfa5e9e6b5b895443c50f154d3b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-08-24 17:11:24 +0900

    行政のCSVファイルをJSONに変換してAPIとして配信できるような処理の作成

--- src/cli.ts
+++ src/cli.ts
@@ -12,13 +12,16 @@ program.storeOptionsAsProperties(false);
 
 program.version(packageJson.version, '-v, --version');
 
+const API_VERSION_NAME = 'v1';
+
 const dataCommand = new Command('data');
 
 dataCommand
   .command('download')
   .description('')
   .action(async (options: any) => {
-    const workbook = XLSX.readFile(path.join('resources', 'master-data', 'download-file-info.csv'));
+    const readFileData = fs.readFileSync(path.join('resources', 'master-data', 'download-file-info.csv'), 'utf8');
+    const workbook = XLSX.read(readFileData, { type: 'string' });
     const sheetNames = Object.keys(workbook.Sheets);
     const themeRows = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[0]]);
     const downloadUrls = themeRows.map((themeRow: any) => {
@@ -46,14 +49,28 @@ dataCommand
   .command('export')
   .description('')
   .action(async (options: any) => {
+    const convertedApiFormatObjs: any[] = [];
     const csvFilePathes = fg.sync(path.join('resources', 'origin-data', '**', '*.csv'));
     for (const csvFilePath of csvFilePathes) {
       const readFileData = fs.readFileSync(csvFilePath, 'utf8');
       const workbook = XLSX.read(readFileData, { type: 'string' });
       const sheetNames = Object.keys(workbook.Sheets);
-      const themeRows = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[0]]);
-      console.log(themeRows);
+      const themeRows: any[] = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[0]]);
+      for(const rowObj of themeRows) {
+        const apiFormat = {
+          category: 'toilet',
+          name: rowObj['施設名'],
+          lat: rowObj['緯度'],
+          lon: rowObj['経度'],
+        }
+        convertedApiFormatObjs.push(apiFormat)
+      }
     }
+    const willSaveFilePath: string = path.join('build', 'api', API_VERSION_NAME, 'category', 'toilet', 'list.json');
+    if (!fs.existsSync(path.dirname(willSaveFilePath))) {
+      fs.mkdirSync(path.dirname(willSaveFilePath), { recursive: true });
+    }
+    fs.writeFileSync(willSaveFilePath, JSON.stringify(convertedApiFormatObjs));
   });
 
 program.addCommand(dataCommand);
