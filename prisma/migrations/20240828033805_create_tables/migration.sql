8dbbc4b618c95be955fe75b8eed9f65a56123dd6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-08-28 12:42:34 +0900

    prisma管理のrelation形式にしないと何かと都合が悪い

--- prisma/migrations/20240828033805_create_tables/migration.sql
+++ prisma/migrations/20240828033805_create_tables/migration.sql
@@ -1,4 +1,13 @@
 -- CreateTable
+CREATE TABLE `category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `category_title_key`(`title`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `place` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
@@ -14,6 +23,3 @@ CREATE TABLE `place` (
     INDEX `place_category_id_idx`(`category_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `place` ADD CONSTRAINT `place_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `category`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
