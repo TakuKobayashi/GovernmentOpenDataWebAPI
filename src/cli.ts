f32f326af3a1e0eb9fdeb73f2a32202ab93dcf57
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-08-28 19:35:51 +0900

    master dataの取り込み用のコマンド作成して実行されるようにした

--- src/cli.ts
+++ src/cli.ts
@@ -23,6 +23,27 @@ const API_VERSION_NAME = 'v1';
 const dataCommand = new Command('data');
 
 dataCommand
+  .command('setup')
+  .description('')
+  .action(async (options: any) => {
+    const downloadInfoFilePath = path.join('resources', 'master-data', 'download-file-info.csv');
+    const downloadUrls: URL[] = [];
+    const categoryTitles: string[] = [];
+    loadSpreadSheetRowObject(downloadInfoFilePath, (sheetName: string, rowObj: any) => {
+      downloadUrls.push(new URL(rowObj.url));
+      if (rowObj.keyword && !categoryTitles.includes(rowObj.keyword)) {
+        categoryTitles.push(rowObj.keyword);
+      }
+    });
+    const newCategoryObj = categoryTitles.map((categoryTitle) => {
+      return {
+        title: categoryTitle,
+      };
+    });
+    await prismaClient.category.createMany({ data: newCategoryObj, skipDuplicates: true });
+  });
+
+dataCommand
   .command('download')
   .description('')
   .action(async (options: any) => {
