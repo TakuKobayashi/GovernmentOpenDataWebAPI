0cd004a77b97619b8cd8998e3b11f85ccef78169
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-09-07 19:52:02 +0900

    データの更新

--- resources/sqls/tables/categories/1/categories_1.sql
+++ resources/sqls/tables/categories/1/categories_1.sql
@@ -1,4 +1,6 @@
 INSERT INTO `categories` (`id`, `title`, `description`) VALUES (1,'toilet','トイレ情報');
 INSERT INTO `categories` (`id`, `title`, `description`) VALUES (2,'trash','ゴミの分別など');
 INSERT INTO `categories` (`id`, `title`, `description`) VALUES (3,'population','人口統計関連');
-INSERT INTO `categories` (`id`, `title`, `description`) VALUES (4,'unknown','未定');
+INSERT INTO `categories` (`id`, `title`, `description`) VALUES (4,'wireless','無線LANなどの設置場所情報');
+INSERT INTO `categories` (`id`, `title`, `description`) VALUES (5,'park','公園の場所情報');
+INSERT INTO `categories` (`id`, `title`, `description`) VALUES (6,'unknown','未定');
